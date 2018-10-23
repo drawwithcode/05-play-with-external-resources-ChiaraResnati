759823cb17162e74e345378b807ecb6218e349ef
chiaraerre <chiara.resnati@github.com>
2018-10-23 19:32:58 +0200

    assignment

--- sketch.js
+++ sketch.js
@@ -1,11 +1,49 @@
-function preload(){
-  // put preload code here
+var mySong;
+var volume;
+var volhistory = [];
+var hhh = 150;
+var x = 0;
+
+function preload() {
+  mySong = loadSound('./assets/Busy_Beat.mp3')
 }
 
 function setup() {
-  // put setup code here
+
+  createCanvas(windowWidth, windowHeight);
+  mySong.play();
+
+  background(0)
+
+
+  analyzer = new p5.Amplitude();
+  analyzer.setInput(mySong);
+
 }
 
 function draw() {
-  // put drawing code here
+
+  background(0, 0, 0, 5)
+
+  volume = analyzer.getLevel();
+  volhistory.push(volume);
+  noFill();
+  stroke(255);
+  beginShape();
+  for (var i = 0; i < volhistory.length; i+= 5) {
+
+    x = i;
+    var y = map(volhistory[i]/2, 0, 1, hhh, 0);
+    vertex(x, y);
+  }
+
+  if(volhistory.length > width) {
+    volhistory = [];
+    hhh += 150;
+  }
+  endShape();
+
+  noStroke();
+  fill(volume+50, volume+50, volume+50, 20)
+  ellipse (width/2, height/2, y*3)
 }
